d580193f7b0cea1062c8de5af367938048de6213
Joseph Price <josephprice747@gmail.com>
2023-03-23 09:31:32 +0000

    add a css file for Themes, has 1 theme

---
+++ client/src/Themes.js
@@ -0,0 +1,5 @@
+// this will be where the user personalises the layout of the app by applying one of the pre-made colour schemes or themes
+
+// themes will apply to all pages
+
+// there should be at least 3 themes
